4f3ccebad37e8ed8d2b5117b6ce3eb3164b890a1
Frank Xu <frankxu0124@gmail.com>
2023-11-28 22:12:10 -0600

    add index.html

--- cam.py
+++ cam.py
@@ -1,35 +1,55 @@
 import threading
 from picamera2 import Picamera2
-from multiprocessing import Process, Manager
 import os
 import cv2
 import time
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 
 os.environ['FLASK_ENV'] = 'development'
 app = Flask(__name__)
 
 
 def gen():
-    """Video streaming generator function."""
     while True:
         frame = cv2.flip(Cam.frame, -1)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.imencode('.jpg', frame)[1].tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        time.sleep(0.01)
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
 def video_feed():
-    # from camera import Camera
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    response = Response(gen(),
-                        mimetype='multipart/x-mixed-replace; boundary=frame')
-    response.headers.add("Access-Control-Allow-Origin", "*")
+    response = Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
     return response
+
+
+@app.route('/requests', methods=['POST', 'GET'])
+def tasks():
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            print(1)
+            pass
+        elif request.form.get('grey') == 'Grey':
+            pass
+        elif request.form.get('neg') == 'Negative':
+            pass
+        elif request.form.get('face') == 'Face Only':
+            pass
+        elif request.form.get('stop') == 'Stop/Start':
+            pass
+        elif request.form.get('rec') == 'Start/Stop Recording':
+            pass
+
+    elif request.method == 'GET':
+        return render_template('index.html')
+    return render_template('index.html')
 
 
 def web_camera_start():
@@ -40,7 +60,6 @@ def web_camera_start():
 
 
 class Cam:
-
     frame = None
 
     @staticmethod
@@ -55,6 +74,7 @@ class Cam:
                 flask_thread.setDaemon(True)
                 flask_thread.start()
             Cam.frame = frame
+
 
 if __name__ == '__main__':
     Cam().camera_start()
