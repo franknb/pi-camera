bad3327c18694a1d670c45bccfeecb3dba3579ba
Frank Xu <frankxu0124@gmail.com>
2023-11-29 09:00:07 -0600

    update index.html

--- cam.py
+++ cam.py
@@ -69,7 +69,7 @@ class Cam:
         picam2 = Picamera2()
         config = picam2.create_preview_configuration(main={"size": (640, 480)}, transform=Transform(hflip=1, vflip=1))
         picam2.configure(config)
-        picam2.start_preview(Preview.DRM)
+        picam2.start_preview()
         picam2.start()
         while True:
             frame = picam2.capture_array()
