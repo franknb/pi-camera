c5dc464fb83652c2ba913adbbbc53b1bfa078e9f
Frank Xu <frankxu0124@gmail.com>
2025-10-05 17:01:54 -0500

    add back class

--- cam.py
+++ cam.py
@@ -189,5 +189,84 @@ class Cam:
         }
 
 
+class YoloV5OnnxDetector:
+    # COCO class names (80 classes)
+    COCO_CLASSES = [
+        'person','bicycle','car','motorcycle','airplane','bus','train','truck','boat','traffic light',
+        'fire hydrant','stop sign','parking meter','bench','bird','cat','dog','horse','sheep','cow',
+        'elephant','bear','zebra','giraffe','backpack','umbrella','handbag','tie','suitcase',
+        'frisbee','skis','snowboard','sports ball','kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket',
+        'bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple','sandwich',
+        'orange','broccoli','carrot','hot dog','pizza','donut','cake','chair','couch','potted plant',
+        'bed','dining table','toilet','tv','laptop','mouse','remote','keyboard','cell phone','microwave',
+        'oven','toaster','sink','refrigerator','book','clock','vase','scissors','teddy bear','hair drier','toothbrush'
+    ]
+
+    def __init__(self, model_path: str, conf_threshold: float = 0.35, iou_threshold: float = 0.45, input_size: int = 640):
+        self.net = cv2.dnn.readNetFromONNX(model_path)
+        try:
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+        except Exception:
+            pass
+        self.conf_threshold = conf_threshold
+        self.iou_threshold = iou_threshold
+        self.input_size = input_size
+
+    def detect(self, image_bgr):
+        h, w = image_bgr.shape[:2]
+        size = self.input_size
+        blob = cv2.dnn.blobFromImage(image_bgr, scalefactor=1/255.0, size=(size, size), mean=(0,0,0), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        try:
+            preds = self.net.forward()  # (1, N, 85)
+        except Exception:
+            return []
+        preds = np.squeeze(preds, axis=0)
+
+        boxes = []
+        confidences = []
+        class_ids = []
+        for det in preds:
+            cx, cy, bw, bh = det[0:4]
+            obj_conf = det[4]
+            class_scores = det[5:]
+            class_id = int(np.argmax(class_scores))
+            class_conf = class_scores[class_id]
+            score = obj_conf * class_conf
+            if score < self.conf_threshold:
+                continue
+            x = int((cx - bw / 2) * w / self.input_size)
+            y = int((cy - bh / 2) * h / self.input_size)
+            width = int(bw * w / self.input_size)
+            height = int(bh * h / self.input_size)
+            boxes.append([x, y, width, height])
+            confidences.append(float(score))
+            class_ids.append(class_id)
+
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.conf_threshold, self.iou_threshold)
+        results = []
+        if len(idxs) > 0:
+            for i in idxs.flatten():
+                x, y, width, height = boxes[i]
+                results.append({
+                    'bbox': [x, y, width, height],
+                    'score': confidences[i],
+                    'class_id': class_ids[i],
+                    'label': self.COCO_CLASSES[class_ids[i]] if class_ids[i] < len(self.COCO_CLASSES) else str(class_ids[i])
+                })
+        return results
+
+    def draw(self, image_bgr, detections):
+        for det in detections:
+            x, y, w_, h_ = det['bbox']
+            x2, y2 = x + w_, y + h_
+            color = (0, 170, 255)
+            cv2.rectangle(image_bgr, (x, y), (x2, y2), color, 2)
+            label = f"{det['label']} {det['score']:.2f}"
+            ((tw, th), _) = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            cv2.rectangle(image_bgr, (x, y - th - 6), (x + tw + 6, y), color, -1)
+            cv2.putText(image_bgr, label, (x + 3, y - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (20, 20, 20), 1, cv2.LINE_AA)
+
 if __name__ == '__main__':
     Cam().camera_start()
